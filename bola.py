7abe3075b5b942e0df3f4a515280e6db5a41d720
theogsb <tgsb@cin.ufpe.br>
2024-07-29 15:24:13 -0300

    Create bola.py

---
+++ bola.py
@@ -0,0 +1,52 @@
+import pygame
+
+class Bola:
+    def __init__(self, win, x, y, raio, velocidade):
+        self.win = win
+        self.x = x
+        self.y = y
+        self.raio = raio
+        self.velocidade = velocidade
+        self.xcord = 1
+        self.ycord = 1
+        self.cor = (255, 255, 255)
+
+    def draw(self):
+        pygame.draw.circle(
+            self.win,
+            self.cor,
+            (self.x, self.y),
+            self.raio
+        )
+    
+    def move(self):
+        self.x += self.velocidade * self.xcord
+        self.y += self.velocidade * self.ycord
+
+        if self.x - self.raio < 0 or self.x + self.raio > self.win.get_width():
+            self.xcord *= -1
+        if self.y - self.raio < 0 or self.y + self.raio > self.win.get_height():
+            self.ycord *= -1
+        
+        
+pygame.init()
+tela = pygame.display.set_mode((800, 600))
+bola = Bola(tela, 400, 300, 5, 0.05)  
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    bola.move()
+
+    tela.fill((40, 40, 40))
+
+    bola.draw()
+
+
+    pygame.display.flip()
+
+pygame.quit()
+exit()
